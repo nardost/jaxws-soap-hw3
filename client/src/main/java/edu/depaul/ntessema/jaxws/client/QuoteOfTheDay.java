b767a4f776f9821e1b19c253ef85b2f16c60eb4b
nardos <32473919+nardost@users.noreply.github.com>
2020-05-08 13:40:04 -0500

    Class file generation with wsimport implemented.

--- client/src/main/java/edu/depaul/ntessema/jaxws/client/QuoteOfTheDay.java
+++ client/src/main/java/edu/depaul/ntessema/jaxws/client/QuoteOfTheDay.java
@@ -1,7 +1,5 @@
 package edu.depaul.ntessema.jaxws.client;
 
-import edu.depaul.ntessema.jaxws.client.generated.*;
-
 public class QuoteOfTheDay {
 
     public static void main(String[] args) {
--- client/src/main/java/edu/depaul/ntessema/jaxws/client/generated/package-info.java
+++
@@ -1,2 +0,0 @@
-@javax.xml.bind.annotation.XmlSchema(namespace = "http://service.jaxws.ntessema.depaul.edu/")
-package edu.depaul.ntessema.jaxws.client.generated;
